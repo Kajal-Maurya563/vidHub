7eeaa129e6db0fd5697b5cfdc8061acedddcc0aa
Kajal Maurya <563kajalmaurya@gmail.com>
2026-09-12 23:13:32 +0530

    Database connected

---
+++ src/index.js
@@ -0,0 +1,45 @@
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: './env'
+})
+
+connectDB();
+
+
+
+
+
+
+
+
+
+
+
+/*
+import mongoose from "mongoose";
+// import express from "express";
+import {DB_NAME} from "./constants";
+const app = express();
+
+( async() =>{
+    try{
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+
+        
+        app.on("error", (error) =>{
+            console.log("ERR: ", error);
+            throw error;
+        })
+
+        app.listen(process.env.PORT, ()=>{
+            console.log(`App is listening on port ${process.env.PORT}`);
+        })
+
+    }
+    catch (error) {
+        console.error("ERROR", error)
+        throw err
+    }
+})()*/
